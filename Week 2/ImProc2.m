e74bfbbdcfb646a0e795fe522f531081286a20c6
tylerpeller <49143891+tylerpeller@users.noreply.github.com>
2020-09-22 11:00:33 -0400

    Add files via upload

---
+++ Week 2/ImProc2.m
@@ -0,0 +1,32 @@
+pic = imread('digital_images_week2_quizzes_lena.png');
+
+
+
+pic = im2double(pic);
+LPF = ones(3,3)*1/9
+imshow(pic);
+
+
+new = imfilter(pic,LPF,'replicate');
+
+Max1 = max(new);
+Max2 = max(Max1)
+
+MSE1 = sum(sum((pic - new).^2))/(256^2)
+PSNR = 10*log10(1/MSE1)
+figure(2);
+% new = im2uint8(new);
+% imshow((new));
+
+
+LPF2 = ones(5,5)*(1/25)
+figure(3);
+new2 = imfilter(pic,LPF2,'replicate');
+imwrite (new2, 'pout2.png');
+% imshow(new2);
+Max3 = max(new2);
+Max4 = max(Max3)
+
+MSE2 = sum(sum((pic - new2).^2))/(256^2)
+PSNR2 = 10*log10(1/MSE2)
+% PSNR__2 = psnr(new,pic,1) 
